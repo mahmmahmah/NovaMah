8468b8ccc6e2300d95471014ed5c72d2ebe80dbd
mahmmahmah <mahmoudahmedamaar12@gmail.com>
2024-09-03 00:37:38 +0300

    Add files via upload

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -30,7 +30,7 @@ const Status = ({
           onClick={() => setStatusMatch("FINISHED")}
           className={`flex-1 px-2 py-1 text-primary text-xs md:text-sm rounded-md ${
             statusMatch === "FINISHED"
-              ? "bg-teal-400 font-semibold"
+              ? "bg-[#443344] font-semibold"
               : "bg-slate-500 font-regular"
           }`}
         >
